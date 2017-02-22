1839f358f2b84440082a3881ea3b100b2634ff34
Mario Vergara Jr <iammariovergarajr@gmail.com>
2017-02-21 21:35:39 -0500

    Configured Python-Flask

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
+@app.route('/index')
 def index():
-	return render_template("index.html")
+    posts = [
+        {
+            'deal': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer placerat velit ac ex sagittis, id placerat nunc luctus. Fusce placerat nibh id tellus ultrices, ac blandit purus sollicitudin. Pellentesque iaculis est vel diam tempor accumsan. Nam tempor nunc ac sapien fermentum porta.'
+        },
+        {
+            'deal': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer placerat velit ac ex sagittis, id placerat nunc luctus. Fusce placerat nibh id tellus ultrices, ac blandit purus sollicitudin. Pellentesque iaculis est vel diam tempor accumsan. Nam tempor nunc ac sapien fermentum porta.'
+        },
+        {
+            'deal': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer placerat velit ac ex sagittis, id placerat nunc luctus. Fusce placerat nibh id tellus ultrices, ac blandit purus sollicitudin. Pellentesque iaculis est vel diam tempor accumsan. Nam tempor nunc ac sapien fermentum porta.'
+        },
+        {
+            'deal': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer placerat velit ac ex sagittis, id placerat nunc luctus. Fusce placerat nibh id tellus ultrices, ac blandit purus sollicitudin. Pellentesque iaculis est vel diam tempor accumsan. Nam tempor nunc ac sapien fermentum porta.'
+        }
+    ]
+    return render_template('index.html', title='Home', posts=posts)
+
+@app.route('/destinations')
+def destinations():
+    return
+
+@app.route('/packageDeals')
+def packageDeals():
+    return
+
+@app.route('/aboutUs')
+def aboutUs():
+    return
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
